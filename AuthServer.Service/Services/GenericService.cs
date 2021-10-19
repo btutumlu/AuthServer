7b23a2a68d92976317c28a9ef651ac072b533f61
btutumlu <b.tutumlu@hotmail.com>
2021-10-19 17:37:45 +0300

    first commit

---
+++ AuthServer.Service/Services/GenericService.cs
@@ -0,0 +1,93 @@
+﻿using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+
+    public class GenericService<TEntity, TDto> : IGenericService<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<TEntity> _genericRepository;
+
+        public GenericService(IUnitOfWork unitOfWork, IGenericRepository<TEntity> genericRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<Response<TDto>> AddAsync(TDto dto)
+        {
+            var newEntity = ObjectMapper.Mapper.Map<TEntity>(dto);
+            await _genericRepository.AddAsync(newEntity);
+            await _unitOfWork.CommitAsync();
+            var newDto = ObjectMapper.Mapper.Map<TDto>(newEntity);
+            return Response<TDto>.Success(newDto, 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> GetAllAsync()
+        {
+            var products = ObjectMapper.Mapper.Map<List<TDto>>(await _genericRepository.GetAllAsync());
+            return Response<IEnumerable<TDto>>.Success(products, 200);
+        }
+
+        public async Task<Response<TDto>> GetByIdAsync(int id)
+        {
+
+            var prdoduct = await _genericRepository.GetByIdAsync(id);
+            if (prdoduct == null)
+            {
+                return Response<TDto>.Fail("ID not found", 404, true)
+;
+            }
+            var productDto = ObjectMapper.Mapper.Map<TDto>(prdoduct);
+            return Response<TDto>.Success(productDto, 200);
+        }
+
+        public async Task<Response<NoDataDto>> Remove(int id)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("ID not found", 404, true);
+            }
+            _genericRepository.Remove(isExistEntity);
+            await _unitOfWork.CommitAsync();
+            return Response<NoDataDto>.Success(200);
+
+        }
+
+        public async Task<Response<NoDataDto>> Update(int id, TDto dto)
+        {
+            var isExistEntity = await _genericRepository.GetByIdAsync(id);
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("ID not found", 404, true);
+            }
+
+            var updateEntity = ObjectMapper.Mapper.Map<TEntity>(dto);
+
+            _genericRepository.Update(updateEntity);
+
+            await _unitOfWork.CommitAsync();
+            return Response<NoDataDto>.Success(200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            var list =  _genericRepository.Where(predicate);
+            var listDto = ObjectMapper.Mapper.Map<IEnumerable<TDto>>(await list.ToListAsync());
+            return Response<IEnumerable<TDto>>.Success(listDto, 200);
+
+        }
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; private set; }
+      
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        public ErrorDto Eror { get; private set; }
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode , IsSuccessful =true};
+        }
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> {Data=default, StatusCode = statusCode , IsSuccessful = true};
+        }
+        public static Response<T> Fail(ErrorDto errorDto , int statusCode)
+        {
+            return new Response<T> { Data = default, Eror=errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Eror = errorDto, StatusCode = statusCode, IsSuccessful =false };
+        }
+    }
+}
